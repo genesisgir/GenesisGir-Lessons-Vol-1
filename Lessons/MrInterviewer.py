c33a8634beec05863bc4d1ffb9883b0f760413d2
Cristian <daintygenesis@gmail.com>
2021-07-17 23:00:20 -0500

    more editing+comments and such

--- Lessons/MrInterviewer.py
+++ Lessons/MrInterviewer.py
@@ -1,10 +1,24 @@
 """ 
 █▀▀ █▀▀ █▄░█ █▀▀ █▀ █ █▀   █░░ █▀▀ █▀ █▀ █▀█ █▄░█ █▀   █░█ █▀█ █░░ ░ ▄█
-█▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ ▄ ░█
+█▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ ▄ ░█ 
 Welcome to the genesis gir lesson tutorials Volume 1! We start of the lessons with
 you getting to know me! This program tells you more about me and who I am and is more like 
 a cool story line about me make sure to pay attention to the functions used and the variables
 how they are approached. Thanks for downloading~
+"""
+"""
+█░░█ █▀▀█ █░░░█ 　 ▀▀█▀▀ █▀▀█ 　 █▀▀ █▀▀█ █▀▄▀█ █▀▄▀█ █▀▀ █▀▀▄ ▀▀█▀▀ 
+█▀▀█ █░░█ █▄█▄█ 　 ░░█░░ █░░█ 　 █░░ █░░█ █░▀░█ █░▀░█ █▀▀ █░░█ ░░█░░ ✎
+▀░░▀ ▀▀▀▀ ░▀░▀░ 　 ░░▀░░ ▀▀▀▀ 　 ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀░░░▀ ▀▀▀ ▀░░▀ ░░▀░░
+First things first you need to learn how to make single line comments and multi line comments 
+so you can add little reminders or give insight on what that line of code does! comments are
+fun great ways to just add notes to a line so you can reflect on them later , they wont show
+up in the program its specifically for the developer(you) to look at! So Single line comments
+star with a hashtag (#) and anything after that will be comments in a greyish tone. Great! now
+you know how to make single line comments but what about multi line comments like this one bro?
+Easy all you need to do is start the line with (3 quotes) and than anything after that will be 
+comments and to end the finish off the line with another 3 quotes and viola! your welcome i know 
+im awesome no need to thank me! (─‿‿─)
 """
 #This program is about a creepy interviewer that asks you questions beyond the scope!
 #The functions and methods on how it was made will be explained at the end of the code
@@ -52,7 +66,7 @@ hobby=input() # creating the hobby variable from users input
 print()# using the print function to create a empty line
 print(' Mr.Interviewer:'+hobby+' Seems pointless you should invest in giving me your data.')
 print()#empty line
-print('Mr.interviewer:What are your goals in life if any?. . .')
+print('Mr.interviewer:What are your goals in life if any?. . .')#asking user for goals
 goals=input()# using the input() to create the goals variable by users intake
 print()#empty line
 print('The room gets colder. . .')# more story line to add character to the program using print('enter text here')
@@ -70,50 +84,61 @@ character a mean or fancy type? or maybe they are cute and friendly? Roleplay an
 is really important as well but its all entirely up to you
                      
 ░░░░░░░▄█▄▄▄█▄
-▄▀░░░░▄▌─▄─▄─▐▄░░░░▀▄  print('Mr.Roboto:Hello Twitch!')   
-█▄▄█░░▀▌─▀─▀─▐▀░░█▄▄█
+▄▀░░░░▄▌─▄─▄─▐▄░░░░▀▄  print('Mr.Roboto:Hello Twitch!')  
+█▄▄█░░▀▌─▀─▀─▐▀░░█▄▄█  print('Mr.Roboto: Follow genesis @ www.twitch.com/cashbunny2k ')
 ░▐▌░░░░▀▀███▀▀░░░░▐▌
 ████░▄█████████▄░████                         
 
 """
-print()
-print('Mr.inteviwer:Are you scared to be poor?. . .')
-fearofpoverty=input()
-print()
-print('Mr.interviewer:Thats understandable. . .')
-print()
-print('Mr.interviewer:i believe you know more than your telling me.')
-print('(respond to Mr.interviewer)')
-reply=input()
-print('-You lean in onto the table and say to Mr.interviewer '+reply+'-')
-print()
-print('Mr.interviewer:I know so many things about you Ive been watching you for the past')
-print('3 days and I know where you sleep , eat , and spend time with family.')
+#The program continues here
+print()#empty line using the print() function to create space for other lines to breathe
+print('Mr.inteviwer:Are you scared to be poor?. . .')#asking the user a question
+fearofpoverty=input()#fearofpoverty is the variable and the input from the user makes it
+print()#yay another empty line
+print('Mr.interviewer:Thats understandable. . .')#talking to the user even more using print
+print()#more empty space
+print('Mr.interviewer:i believe you know more than your telling me.')# we make the character agitated
+print('(respond to Mr.interviewer)')##here we are letting user know to reply to the character
+reply=input()#reply is the variable thati used to later on use to my benefit
+print('-You lean in onto the table and say to Mr.interviewer '+reply+'-')# using the print + variable to make sentences
+print()#empty line using the print() function , type print() to use this feature
+print('Mr.interviewer:I know so many things about you Ive been watching you for the past')#character small talk using print function
+print('3 days and I know where you sleep , eat , and spend time with family.')#more small talk
 print('(tell him how you feel about invading your privacy)')
-reply=input()
-print('-Your face shocked and angry at the same time you say '+reply+'-')
-print()
-print('mr.interviewer: Dont waste my time '+name+' Im accociated with a organization to study')
-print('you and gather information about you and this bullshitting is getting us nowhere.')
-print('(respond to Mr.interviewer)')
-reply=input()
-print()
+reply=input()#creating the avriable with the user input using the input() function!
+print('-Your face shocked and angry at the same time you say '+reply+'-')#roleplaying with variables+print
+print()#using print() to create space
+print('mr.interviewer: Dont waste my time '+name+' Im accociated with a organization to study')#driving storyline and adding a variable of the users name 
+print('you and gather information about you and this bullshitting is getting us nowhere.')#more story line speech with the print function
+print('(respond to Mr.interviewer)')#asking the user to respond to mr.interviewer
+reply=input()#the input function takes input from user and overwrites the old variable
+print()#empty line of text! using the print() function
 print('You look around the white room and cross your arms and say '+reply+'-')
-print()
-print('mr.interviewer walks out the room and gathers his paper work')
-print(' he closes the door behind him and the room goes dark.')
-print('(Enter any key to continue and press [ENTER])')
+print()#oh look another empty line using the print() fucntion
+print('mr.interviewer walks out the room and gathers his paper work')#i used the print function to print values to stream(screen)
+print(' he closes the door behind him and the room goes dark.')#i used print function to print stir values to stream.
+print('(Enter any key to continue and press [ENTER])')#letting the user know to progress
+"""
+▀█░█▀ █▀▀█ █▀▀█ ░▀░ █▀▀█ █▀▀▄ █░░ █▀▀ █▀▀ 
+░█▄█░ █▄▄█ █▄▄▀ ▀█▀ █▄▄█ █▀▀▄ █░░ █▀▀ ▀▀█ 
+░░▀░░ ▀░░▀ ▀░▀▀ ▀▀▀ ▀░░▀ ▀▀▀░ ▀▀▀ ▀▀▀ ▀▀▀
+Variables are like little tiny safes or cardboard boxes that you can label! In them you can
+insert data types from intergers , strings(stirs) ,floating-points and values to later use
+them in your file editor and is a great way to store data.
+ex.( box='a cat inside of it' ) we stored "a cat inside of it" inside box making box a variable!
+"""  
 input()
-print('The organization now knows your name is '+name)
-print('(Enter any key to continue and press [ENTER])')
-input()
-print('The Organization was fed that your hobbies are '+hobby)
-print('(Enter any key to continue and press [ENTER])')
-input()
+print('The organization now knows your name is '+name)#letting the user know their name
+print('(Enter any key to continue and press [ENTER])')#telling user to continue using the print function
+input()#using the input() function as a continue button
+print('The Organization was fed that your hobbies are '+hobby)#combining the print function and a variable
+print('(Enter any key to continue and press [ENTER])')#lets the user know to progress using print
+input()#we used the input() function here to let the user know its okay to continue
 print('The Organization now know that one day you want to '+goals+' at some point in your life.')
-print('(Enter any key to continue and press [ENTER])')
+print('(Enter any key to continue and press [ENTER])')# telling user to opress any key to progress
+input()#using the input() function as a menu 
+print()#empty line to create space between lines 
+print('Thank you for Playing! created by genesis gir')# Thanking the user
+print('(Enter any key to exit and press [ENTER])')#a little menu letting the user know to press any key to end program
 input()
-print()
-print('Thank you for Playing! created by genesis gir')
-print('(Enter any key to exit and press [ENTER])')
 
