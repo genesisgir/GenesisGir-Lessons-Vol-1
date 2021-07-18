553809f82f056b815562ee6bde313210a9e6b2b4
Cristian <daintygenesis@gmail.com>
2021-07-17 19:10:11 -0500

    New Upload

---
+++ HiyaImGenesis.py
@@ -0,0 +1,70 @@
+""" 
+█▀▀ █▀▀ █▄░█ █▀▀ █▀ █ █▀   █░░ █▀▀ █▀ █▀ █▀█ █▄░█ █▀   █░█ █▀█ █░░ ░ ▄█
+█▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ ▄ ░█
+Welcome to the genesis gir lesson tutorials Volume 1! We start of the lessons with
+you getting to know me! This program tells you more about me and who I am and is more like 
+a cool story line about me make sure to pay attention to the functions used and the variables
+how they are approached. Thanks for downloading~
+"""# multi line comments pretty useful if you wanna explain yourself
+print('Hiya! Im genesis! Lets get to know eachother whats your name?')# greeting the user and we use the print function to do this!
+name=input()#takes input of the name variable to use later on in the program
+print('Nice to meet you '+name+' I think it would be nice for us to get to know eachother better?')# using the print function to print the string values onto the stream (screen)
+print()#empty  
+print('My names Cristian and I started doing programming about 7 days ago!')# using print to tell user more about myself
+print('and everyday im learning something new not only that I think its imporatant to train.')# more chatter with the print function!
+print('The world of coding is so beautiful to me and I find myself lost doing it ALOT!')# print function being used here to generate text on stream(screen)
+print()#empty
+print(' What File editor do you use? i use visual studio code!')# asking the user what text editor they use!
+print()#empty
+file_editor=input()# input makes users file editor name using the input() function use it if your asking someone a question!
+print()#empty
+print(' Oh wow I would love to try '+file_editor+' one day who knows!')# here we use the file_editor variable and use it to reveal their tool of choice
+print()#empty
+print('I was born in New York city, Queens and its pretty nice up there')# yes its true i was born in NYC also print call function
+print('Im from Baltimore and its too loud over here and i miss my country peru')# telling the user that i orgin from peru
+print('Thats where my parents are from and the food there is amazing!')# using the print call function to display text
+print()# another cute empty line! wanna do this just use the print() cal function
+print('Anyways where are you from?') # asking the user where are they from
+print()# empty line
+location=input()# making the users input the location to be stored in the variable
+print()# another empty line of space to make the texts have room
+print(location +' Sounds nice, Id like to go travel there sometime. I love traveling!')# using the location variable to reveal where they are from!
+print()# empty line space 
+print('Im also into spicy foods and im spanish as well! and Im a huge gamer.')# more chit chat with print()
+print()# giving room for the text to breathe by making an empty line
+print('Whats your favorite video game?')# asking user what their favorite is game is with the print function
+game=input()# users input becomes game variable and is later used later down the program
+print()# empty line use the print() call function to use this function
+print(game+ '?'+' hmm sounds nice I should try it some time hehe')# using print() to have a friendly conversation
+print()# empty line
+print(' Im glad that we got to know eachother see you in the next tutorial '+name+' see ya!')# using print and a variable in tandem to createa atmosphere
+print()# i used the print() call function to create some more room between the lines
+print('genesis gathered that your name is '+name+'.') # telling the user that we know their name based off variables
+print('genesis gathered that your from '+location+'.') # telling the user that we know their location based off variables
+print('genesis gathered that you like video games like '+game+'.')# telling the user  that we know what game they like based off variables
+print()# an ampty void of text and a long line of nothing created with the print() func
+ # the program exits here (42 lines in total!)
+"""" 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂
+Did you enjoy the little program i made? did it teach you more about coding or meI hope so because make sure to study and research this source code over and over until you understand
+ its functions and methods! We used variables , functions , and clever creative ways to interact
+ with user and learn about them and what they like and share about ourselves as well. 
+ pretty remarkable id say. below are the functions used and what they do
+ Variables Think of them like a cardboard box you can box your data types in!
+ they are very handy when you need to gather information or even capture a users input and later on
+ reveal the variable in the program for whatever your reason. variables store data like intergers 
+ floats, strings! pretty essential!
+ input prints the value to stream and takes input from the user on keyboard and enter
+ the input function can be used in alot of creative ways, insert it when you want to gather a users answer
+ and store them in variables for later use. Ask a question in print then after that link make a variable
+ with an assignment operator and put input it will take the users input whatever they texted.
+ links below!
+ -twitch: https://www.twitch.tv/cashbunny2k follow , chat , and learn!
+ -github: https://github.com/GenesisGir programs , learn , contribute , a great dev that loves his community!
+ 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂
+ ──────▄▀▄─────▄▀▄
+─────▄█░░▀▀▀▀▀░░█▄
+─▄▄──█░░░░░░░░░░░█──▄▄
+█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█ oh and cotal says hi!
+
+ """
+ 
