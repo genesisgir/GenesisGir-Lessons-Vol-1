e07bc31a7df65f9ac9e3e7ce9d6c50fe184d1308
Cristian <daintygenesis@gmail.com>
2021-07-17 22:11:58 -0500

    Working on the Mr.interviewer .py and its going great!

---
+++ Lessons/MrInterviewer.py
@@ -0,0 +1,119 @@
+""" 
+█▀▀ █▀▀ █▄░█ █▀▀ █▀ █ █▀   █░░ █▀▀ █▀ █▀ █▀█ █▄░█ █▀   █░█ █▀█ █░░ ░ ▄█
+█▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ ▄ ░█
+Welcome to the genesis gir lesson tutorials Volume 1! We start of the lessons with
+you getting to know me! This program tells you more about me and who I am and is more like 
+a cool story line about me make sure to pay attention to the functions used and the variables
+how they are approached. Thanks for downloading~
+"""
+#This program is about a creepy interviewer that asks you questions beyond the scope!
+#The functions and methods on how it was made will be explained at the end of the code
+# functions , variables mostly enjoy the code Thanks for downloading!
+print('The door opens and a man in a suit walks threw.')#using the print function to print values onto the screen
+""" The print function is used to print the values in the containers onto the stream(screen)
+and in this case to generate text to the program. Its pretty useful and i first start off by
+thinking what i want the program to be about and use creative little ideas to conjure up plots
+in this case i chose to make a program that interviews you that being Mr.interviewer that asks
+you random questions with a creepy horror type scence set to it!
+"""
+print()# using the print() to create a space between the lines of code you can do this by typing print() into the line
+print('hes wearing a black suit and red tie but his face is strangely blank and white')#using print function to type more values onto screen
+"""
+█░█ █▀█ █░█░█   ▀█▀ █▀█   █░█ █▀ █▀▀   █▀█ █▀█ █ █▄░█ ▀█▀
+█▀█ █▄█ ▀▄▀▄▀   ░█░ █▄█   █▄█ ▄█ ██▄   █▀▀ █▀▄ █ █░▀█ ░█░
+The print function always comes with two parentheses and any function in that matter and we 
+enter what data types we want inside of them and everything inside of it is called an
+argument and the print is called a function and in this case im using a (stir) or better known
+as a string to make a storyline! so print(    ) and anything inside it will be printed onto 
+stream(screen) just make sure its a correct data type: interger , float or a string value.
+""" 
+print('He sits down at your table as you both are surrounded by white walls') 
+print('in a errie bright white room.')# letting the user know what setting they are in!
+print('The room gets colder and colder and he begins to stare at you and utters the words.')
+#line 32 By saying the room gets colder we let the user know its horror themed.
+print()# an empty line of text using the print() function!
+print('Mr.Interviewer: Whats your name sir?')#asking the user their name.
+name=input()# by entering input() we let the users intake become the variable
+#The """ create a multi line comment as you can see below to create multiple lines of comments
+#very useful if you need to leave larger comments but make sure to end it with another """ on the bottom
+"""
+█░█ █▀█ █░█░█   ▀█▀ █▀█   █░█ █▀ █▀▀   █ █▄░█ █▀█ █░█ ▀█▀
+█▀█ █▄█ ▀▄▀▄▀   ░█░ █▄█   █▄█ ▄█ ██▄   █ █░▀█ █▀▀ █▄█ ░█░
+input() waits for the users keyboard input and when they press enter it takes that input
+this can be very essential to make variables by the user and can be used to your advantage
+make sure to pratice input() often! and in line 33 im using the input() function
+to create a variable using the users input to later use it in the program
+"""
+print()# empty line of text created using the print() function to put spaces between lines
+print('Mr.interviewer:Okay '+name+' Im going to be asking you some Q&A.')#using the print again to print values
+print()# an empty void of line
+print('Mr.Interviewer:Whats your favorite thing to do currently?. . .')#asking them whats their hobby is
+hobby=input() # creating the hobby variable from users input
+print()# using the print function to create a empty line
+print(' Mr.Interviewer:'+hobby+' Seems pointless you should invest in giving me your data.')
+print()#empty line
+print('Mr.interviewer:What are your goals in life if any?. . .')
+goals=input()# using the input() to create the goals variable by users intake
+print()#empty line
+print('The room gets colder. . .')# more story line to add character to the program using print('enter text here')
+print()#empty line
+print('Mr.interviewer:These goals must be important to you '+name+'.')
+"""
+
+█▀▀ █ █░█ █ █▄░█ █▀▀   █▀▀ █░█ ▄▀█ █▀█ ▄▀█ █▀▀ ▀█▀ █▀▀ █▀█
+█▄█ █ ▀▄▀ █ █░▀█ █▄█   █▄▄ █▀█ █▀█ █▀▄ █▀█ █▄▄ ░█░ ██▄ █▀▄
+Notice on how we are brining life to the character by making him ask redimentary questions
+giving the feel that you are connected with mr.interviewer and giving a sense of personality
+very important to be creative during the writing process. Choose a personality for your character!
+which is the fun part you can create all type of personalities using your brain meat. Is the
+character a mean or fancy type? or maybe they are cute and friendly? Roleplay and imagination
+is really important as well but its all entirely up to you
+                     
+░░░░░░░▄█▄▄▄█▄
+▄▀░░░░▄▌─▄─▄─▐▄░░░░▀▄  print('Mr.Roboto:Hello Twitch!')   
+█▄▄█░░▀▌─▀─▀─▐▀░░█▄▄█
+░▐▌░░░░▀▀███▀▀░░░░▐▌
+████░▄█████████▄░████                         
+
+"""
+print()
+print('Mr.inteviwer:Are you scared to be poor?. . .')
+fearofpoverty=input()
+print()
+print('Mr.interviewer:Thats understandable. . .')
+print()
+print('Mr.interviewer:i believe you know more than your telling me.')
+print('(respond to Mr.interviewer)')
+reply=input()
+print('-You lean in onto the table and say to Mr.interviewer '+reply+'-')
+print()
+print('Mr.interviewer:I know so many things about you Ive been watching you for the past')
+print('3 days and I know where you sleep , eat , and spend time with family.')
+print('(tell him how you feel about invading your privacy)')
+reply=input()
+print('-Your face shocked and angry at the same time you say '+reply+'-')
+print()
+print('mr.interviewer: Dont waste my time '+name+' Im accociated with a organization to study')
+print('you and gather information about you and this bullshitting is getting us nowhere.')
+print('(respond to Mr.interviewer)')
+reply=input()
+print()
+print('You look around the white room and cross your arms and say '+reply+'-')
+print()
+print('mr.interviewer walks out the room and gathers his paper work')
+print(' he closes the door behind him and the room goes dark.')
+print('(Enter any key to continue and press [ENTER])')
+input()
+print('The organization now knows your name is '+name)
+print('(Enter any key to continue and press [ENTER])')
+input()
+print('The Organization was fed that your hobbies are '+hobby)
+print('(Enter any key to continue and press [ENTER])')
+input()
+print('The Organization now know that one day you want to '+goals+' at some point in your life.')
+print('(Enter any key to continue and press [ENTER])')
+input()
+print()
+print('Thank you for Playing! created by genesis gir')
+print('(Enter any key to exit and press [ENTER])')
+
