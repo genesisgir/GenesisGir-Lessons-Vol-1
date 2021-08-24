d8be55d901032fcbf0527d25e8893f4e02dab1fd
Genesis <daintygenesis@gmail.com>
2021-08-24 00:27:42 -0500

    Added introduction header to .py

--- Lessons/30DaysOnMars/30DaysOnMars.py
+++ Lessons/30DaysOnMars/30DaysOnMars.py
@@ -1,7 +1,13 @@
 """ 
 █▀▀ █▀▀ █▄░█ █▀▀ █▀ █ █▀   █░░ █▀▀ █▀ █▀ █▀█ █▄░█ █▀   █░█ █▀█ █░░ ░ ▄█
 █▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ ▄ ░█ 
-Welcome to the genesis gir lesson tutorials Volume 1! 
-. Thanks for downloading!
+Welcome to the genesis gir lesson tutorials Volume 1! 30 DAYS ON MARS is a program that takes
+the user into a remote planet on mars to fend off for themselves with viral infections and mutants
+that have invaded and over populated the infastrutcure and facilty find from the creator of SOS brings
+in new experience fight your way out of the infection and hopefully find your teamates that are in desprate
+need of your aid. NASA has issued a investigation of the planet due to its loss of communication with
+the last team and Travis has to go in and find them. The viral infections has rendered the other nasa explorers
+unmarked and unfamiliar with cinamatic literature crafted in a way to have you out your seat. Death defying
+and cunning take the user into a out of this world experience Thanks for downloading~
 ⼕ㄖᗪ🝗ᗪ & 山尺讠セセ🝗𝓝 ⻏丫 Ꮆ🝗𝓝🝗丂讠丂 Ꮆ讠尺
 """
