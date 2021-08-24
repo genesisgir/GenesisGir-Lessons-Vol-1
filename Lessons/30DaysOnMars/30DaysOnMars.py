a7b78ff48735cad1a2b2bba629bcf48942823717
Genesis <daintygenesis@gmail.com>
2021-08-24 04:25:41 -0500

    Adding variables + constants to the actual program!

--- Lessons/30DaysOnMars/30DaysOnMars.py
+++ Lessons/30DaysOnMars/30DaysOnMars.py
@@ -11,3 +11,229 @@ unmarked and unfamiliar with cinamatic literature crafted in a way to have you o
 and cunning take the user into a out of this world experience Thanks for downloading~
 ⼕ㄖᗪ🝗ᗪ & 山尺讠セセ🝗𝓝 ⻏丫 Ꮆ🝗𝓝🝗丂讠丂 Ꮆ讠尺
 """
+#----------------------------------- 𝐌𝐀𝐈𝐍 𝐏𝐋𝐀𝐘𝐄𝐑
+
+# Main Player
+travis='Travis' # The main Character in the game
+
+
+
+"""
+🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂
+
+
+█░█░█ █░█ █▀█   █ █▀   ▀█▀ █▀█ ▄▀█ █░█ █ █▀   █▀█ █ █░░ █▀▀ █▄█
+▀▄▀▄▀ █▀█ █▄█   █ ▄█   ░█░ █▀▄ █▀█ ▀▄▀ █ ▄█   █▀▄ █ █▄▄ ██▄ ░█░
+
+Travis riley is a NASA astronaut that was sent out on a mission to find the rest of his team that went missing
+on a gone wrong mission to mars. His close bestfriend Roger is comming along for the ride but. You are a NASA
+commander pilot and your job as team leader of NASA BRAVO B is to find and recover your team.
+
+🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂
+
+"""
+
+#----------------------------------- 𝐒𝐓𝐎𝐑𝐘 𝐂𝐇𝐀𝐑𝐀𝐂𝐓𝐄𝐑𝐒
+
+# story characters
+monica='Monica (Story NPC)'# One of Travis's old military ex girlfriends
+roger='Roger (Story NPC)' # Travis's old cambodia war comradre
+dennis='Dennis (Story NPC)' # One of the explorers sent out to discover the missing staff
+nasa_commander=' Stephan NASA Commander (Story NPC)'# The commander of the entire NASA mission
+taylor='Taylor the photograpgher (Story NPC)' # Sent by NASA to document the mission
+
+#----------------------------------- 𝐈𝐍𝐅𝐄𝐂𝐓𝐄𝐃 
+
+# Infected
+mutant_infected_type_b='Infected NASA Explorer (Type B)' # One of the explorers from the last mission sent to discover gone bad
+mutant_infected_type_a='Infected NASA Engineer (Type A)' # NASA engineer turned infected
+mutant_infected_type_z='Infected NASA Pilot (Type Z)' # NASA pilot turned infected 
+mutant_infected_type_x='Infected NASA researcher (Type X)' # One of the researchers turned infected sadly
+mutant_infected_type_k='Infected NASA Chef (Type K)' # A Nasa spaceship chef that turned
+
+# Infected Killcount
+mutant_infected_type_b_killcount = 0
+mutant_infected_type_a_killcount = 0
+mutant_infected_type_z_killcount = 0
+mutant_infected_type_x_killcount = 0
+mutant_infected_type_k_killcount = 0
+
+
+#----------------------------------- 𝐈𝐓𝐄𝐌𝐒
+
+# Items
+flashlight='Modern Flashlight (Item)'
+bic_lighter='Bic lighter (Item)' # A lighter that can be used to ignite flammable liquids
+nasa_watch='NASA Standard Watch (Item)' # Tells user the time on mars
+jar='Empty Glass Jar (Item)' # Used to store liquids inside
+cloth='Piece of cloth (Item)'
+walkie='NASA Walkie Talkie (Item)'
+
+# Item Amounts
+flashlight_amount = 0
+bic_lighter_amount = 1
+nasa_watch_amount = 1
+jar_amount = 0
+cloth_amount = 0
+walkie_amount = 0
+
+#----------------------------------- 𝐑𝐄𝐒𝐎𝐔𝐑𝐂𝐄𝐒
+ 
+# Resources
+oxygen_tank='Oxygen Tank (Resource)' # used to fill up air supplies to a users NASA spacesuit
+gasoline_tank='Gasoline Tank (Resource)' # Used for a survival situation or to explode with weapons
+shotgun_shell_box='Shotgun Shell Ammunition Box (Resource)' # Ammunition for shotgun gun type weapons
+machinegun_ammunition_box='Machine Gun Ammunition Box (Resource)'
+bat='Batteries ++ A box (Resource)'
+bile='Infected Bile Liquid (Resource)' # Bile from the infected can be used in crafting
+
+# resource Amounts
+oxygen_tank_amount = 0
+gasoline_tank_amount = 0
+shotgun_shell_box_amount = 0
+bat_amount = 0
+bile_amount = 0
+
+#----------------------------------- 𝐂𝐋𝐎𝐓𝐇𝐈𝐍𝐆
+
+# Clothing
+nasa_suit='NASA Spacesuit (Clothing)' # Keeps the user alive on the atmosphere of mars
+hazmat='Hazmat Biohazzard suit (Clothing)' # a suit designed to quarantine the user from viral infection
+sub_zero_suit='Sub ZERO explorer suit (clothing)' # used to navigate threw sub zero tempatures on mars
+fire_hazzard='Fire Hazzard Suit (clothing)' # protect yourself from flames and etting burnt to a crisp
+
+# Clothing Amounts
+nasa_suit_amount = 0
+hazmat_amount = 0
+sub_zero_suit_amount = 0
+fire_hazzard_amount = 0 
+
+
+#----------------------------------- 𝐖𝐄𝐀𝐏𝐎𝐍𝐒
+
+# Weapons
+shotgun='12 gauge shotgun (Weapon)' # powerful modern shotgun used for extermination 
+super_shotgun='Super Shotgun (Weapon)' # a powerful shotgun used to blow enemies away 
+bayonet_knife='Military Cambodia Knife (Weapon)' # used to gut enemies
+nasa_plasma_pistol='NASA Plasma Pistol (Weapon)' # A Plasma pistol issued by NASA for self defense
+chainsaw='Rusty Chainsaw (Weapon)' # used to tear open enemies to shreds or doors
+
+"""
+🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 
+
+▀█▀ █░█ █▀▀   █▀▀ █░█ ▄▀█ █ █▄░█ █▀ ▄▀█ █░█░█
+░█░ █▀█ ██▄   █▄▄ █▀█ █▀█ █ █░▀█ ▄█ █▀█ ▀▄▀▄▀ 
+the Chainsaw is a compact weaopon that can rip enemies to shreds with its blood curdling sound and was only ment
+to break into martian rock and caves but in this survival situation using it against infected to survive is choice
+
+𝐓𝐢𝐩: 𝐊𝐢𝐥𝐥 𝐢𝐧𝐟𝐞𝐜𝐭𝐞𝐝 𝐚𝐧𝐝 𝐰𝐚𝐭𝐜𝐡 𝐨𝐫𝐠𝐚𝐧𝐬 𝐟𝐥𝐲
+
+🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂
+"""
+sawed_off='Sawed off Shotgun (Weapon)' # pump the enemies one time in the face or body with this deadly makeshift gun
+
+#Weapon Amounts
+shotgun_amount = 0
+super_shotgun_amount = 0
+bayonet_knife_amount = 0
+nasa_plasma_pistol_amount = 0
+chainsaw_amount = 0
+sawed_off_amount = 0
+
+#----------------------------------- 𝐓𝐑𝐀𝐏𝐒
+
+# Traps
+bear_trap='Deadly makeshift bear Trap (Trap)' # A trap that takes the infected and severes their ankle to the bone
+
+
+
+#----------------------------------- 𝐓𝐇𝐑𝐎𝐖𝐀𝐁𝐋𝐄𝐒
+
+# Throwables
+pipe_bomb='Ticking Pipe Bomb (Throwables)'
+molotov='Molotov Bottle (Throwables)'
+
+#Throwables Amounts
+pipe_bomb_amount = 0
+molotov_amount = 0
+
+#----------------------------------- 𝐇𝐄𝐀𝐋𝐈𝐍𝐆
+
+# Healing
+small_medi_kit=' Samll Medkit (Healing)'# used to heal when user is weak
+full_medikit='Large medkit (Healing)'
+syringe='Antiseptic Syringe (Healing)' # used if user is infected with the viral infection B-10
+band='Bandage aids (Healing)'
+pills='Antibiotic Pills Mozenatrazine (Healing)'
+
+
+#----------------------------------- 𝐂𝐎𝐍𝐒𝐔𝐌𝐀𝐁𝐋𝐄𝐒
+
+# Consumables
+snack='Bag of Chips (Consumable)' # a snack used to heal 
+soda='Soda can (Consumable)' # Can be found scattered in the facilities
+canned_meat='Canned Lunch Tin (Consumable)' # used to gain a fair amount of health
+energy_bar='Crunchy Energy bar (Consumable)' # A crunchy energy bar that heals user
+
+
+#----------------------------------- 𝐈𝐍𝐅𝐄𝐂𝐓𝐈𝐎𝐍 𝐓𝐘𝐏𝐄𝐒 ☣
+
+#Infection Types
+infection_type_B_10='B-10 (VIRUS)' # one of the many virus types user or anyone can get infected by
+infection_type_B_11='B-11 (VIRUS - imminent death)' # The worst type of infection you can receive and tehre is no cure you will DIE.
+
+
+#----------------------------------- 𝐕𝐄𝐇𝐈𝐂𝐋𝐄𝐒
+
+# Vehicles
+moonwalker='NASA B-1080 Moon convoy (Vehicle)' # Slow but contains a healing station for inections onboard
+tracer='Tracer Mark II (Vehicle)' # A very fast tracer vehicle to drive around the mars surfcae
+rounder='Rounder sphere (vehicle)'# A round vehicle that rolls // can be equipped with death spikes
+
+
+#----------------------------------- 𝐂𝐑𝐀𝐅𝐓𝐀𝐁𝐋𝐄
+
+# Craftable
+pipebomb='Ticking Pipebomb (Craftable)' # 
+makeshift_bear_trap='Make Shift Bear Trap (Craftable) '# break some ankles!
+molotov='Leaking Molotov (Craftable)' # Create a fiery throwable to ignite infected!
+bile='Bile Jar (Cratable)' # Create a jar that makes the infected kill one another!
+
+
+#----------------------------------- 
+
+
+
+print()
+print()
+print()
+print()
+print('██████╗░░█████╗░  ██████╗░░█████╗░██╗░░░██╗░██████╗  ░█████╗░███╗░░██╗  ███╗░░░███╗░█████╗░██████╗░░██████╗')
+print('╚════██╗██╔══██╗  ██╔══██╗██╔══██╗╚██╗░██╔╝██╔════╝  ██╔══██╗████╗░██║  ████╗░████║██╔══██╗██╔══██╗██╔════╝')
+print('░█████╔╝██║░░██║  ██║░░██║███████║░╚████╔╝░╚█████╗░  ██║░░██║██╔██╗██║  ██╔████╔██║███████║██████╔╝╚█████╗░')
+print('░╚═══██╗██║░░██║  ██║░░██║██╔══██║░░╚██╔╝░░░╚═══██╗  ██║░░██║██║╚████║  ██║╚██╔╝██║██╔══██║██╔══██╗░╚═══██╗')
+print('██████╔╝╚█████╔╝  ██████╔╝██║░░██║░░░██║░░░██████╔╝  ╚█████╔╝██║░╚███║  ██║░╚═╝░██║██║░░██║██║░░██║██████╔╝')
+print('╚═════╝░░╚════╝░  ╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░  ░╚════╝░╚═╝░░╚══╝  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░')
+print('                .                                            .                                             ')
+print('     *   .                  .              .        .   *          .                            ,          ')
+print('  .         .                     .       .           .      .        .                                    ')
+print('        o                             .                   .                           .                    ')
+print('         .              .                  .           .                                                   ')
+print('          0     .                                                                         ,                ')
+print('                 .          .                 ,                ,    ,                            .         ')
+print(' .          \          .                         .                                *                        ')
+print('      .      \   ,                                                                           .             ')
+print('   .          o     .                 .                   .                  .          ,                  ')
+print('     .         \                 ,             .                .                                          ')
+print('               #\##\#      .                              .        .                                       ')
+print('             #  #O##\###                .                        .               .                         ')
+print('   .        #*#  #\##\###                       .                     ,                          .         ')
+print('        .   ##*#  #\##\##               .                     .                         .                  ')
+print('      .      ##*#  #o##\#         .                             ,                                          ') 
+print('          .     *#  #\#     .                    .             .                                   .       ')   
+print('                      \          .                         .                                 .             ')
+print('____^/\___^--____/\____O______________/\/\---/\___________---________________________/\____________________')
+print('   /\^   ^  ^    ^                  ^^ ^  \ ^          ^       ---                         .         .     ')
+print('         --           -            --  -      -         ---  __       ^         --     .          .        ')
+print('   --  __                      ___--  ^  ^                         --                       --_            ')
+print()
