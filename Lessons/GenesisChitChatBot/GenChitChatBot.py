3c356cc4e3b71bd28502107562dcc2264ddbd4f4
Genesis Gir <daintygenesis@gmail.com>
2021-08-29 23:23:01 -0700

    New release! Come Try out The Gen Chit-Chat Bot today!
    Finished the program this is my first program in awhile and its finally done make sure to drop a follow if you enjoyed this it uses basic programming knowledge from chapter 1! Thank you for watching to my 3 watchers!

--- Lessons/GenesisChitChatBot/GenChitChatBot.py
+++ Lessons/GenesisChitChatBot/GenChitChatBot.py
@@ -41,8 +41,60 @@ print('(loading. . .)')
 print('(press enter)')
 input()
 
+"""
+
+                                      █░█ ▄▀█ █▀█ █ ▄▀█ █▄▄ █░░ █▀▀ █▀
+                                      ▀▄▀ █▀█ █▀▄ █ █▀█ █▄█ █▄▄ ██▄ ▄█
+
+variables are like tiny little safes or storage units you can store values and data types inside to later use them in your
+program ofr ease of use! variables are really useful if you want to store data inside them from integers , floats,
+string data types and even Boolean values like True and false can be used and stored within them. In this program
+the varaible 'prompt' is storing '(press enter)' so that way we dont have to type it all the time or at least thats why
+im using it. variables are created using assignment statements that contain the variable name the assignment operator
+and the value or data type to be stored within the variable whatever you named the variable it must have valid names or
+you will get a syntax error, again a variable cant have any spaces or begin with a number or contain special characters
+in it like '!@#$%' etc. so make sure to name your variables something in the valid formats. 
+
+Pick the right variable name that has to do with the item or variable to begin with like the automatetheboring stuff if you
+label all of your moving boxes stuff you wont ever find anything to picking the right now that will suit your project or 
+program will best interest your organization later down the road!
+
+
+                                                   ██████                                                                                      
+                                               ▓▓▓▓      ▓▓░░                                                                       
+                                           ░░░░              ▓▓▓▓                                                         
+                                       ████       𝕧𝕒𝕣𝕚𝕒𝕓𝕝𝕖𝕤       ████                                       
+                                   ████                              ████                                  
+                               ████                 True                 ████                          
+                           ████                                              ████               
+                           ██  ████                 False                ████  ▓▓             
+                           ██      ▓▓▓▓                              ▓▓▓▓      ▓▓              
+                           ██          ▓▓▓▓                      ▓▓▓▓          ▓▓              
+                           ██              ▓▓▒▒              ▓▓▓▓              ▓▓                
+                           ██                  ▓▓▓▓      ▓▓▒▒          78      ▓▓            
+                           ██    'strings'         ██████                      ▓▓                
+                           ██                        ▓▓         1              ▓▓           
+                           ██                        ▓▓           integers     ▓▓                
+                           ██                        ▓▓                        ▓▓              
+                           ██              DATA      ▓▓                        ▓▓              
+                           ██                        ▓▓          500.1         ▓▓               
+                           ██    'Cat'             ▓▓▓▓▓▓             (floats) ▓▓               
+                           ██                  ▓▓▓▓  ▓▓  ▓▓▒▒                  ▓▓              
+                           ██              ▓▓▒▒      ▓▓      ▓▓▓▓              ▓▓                
+                           ██          ▓▓▓▓          ▓▓          ▓▓▓▓          ▓▓              
+                           ██      ▒▒▒▒░░░░          ▓▓    2.1     ░░▒▒▒▒      ▓▓                
+                           ██  ▒▒▒▒░░░░              ▓▓              ░░░░▒▒▒▒  ▓▓                
+                           ████         Your Mind    ▓▓                      ██▓▓                
+                               ████                  ▓▓   95             ████      𝙏𝙞𝙥: 𝙡𝙖𝙗𝙚𝙡 𝙮𝙤𝙪𝙧 𝙫𝙖𝙧𝙞𝙖𝙗𝙡𝙚𝙨 𝙨𝙤𝙢𝙚𝙩𝙝𝙞𝙣𝙜 𝙩𝙝𝙖𝙩 𝙝𝙖𝙨 𝙩𝙤 𝙙𝙤 𝙬𝙞𝙩𝙝 𝙩𝙝𝙚 𝙩𝙤𝙥𝙞𝙘                    
+                                   ▓▓▓▓              ▓▓              ▓▓▓▓                                        
+                                       ▓▓▓▓          ▓▓          ▓▓▓▓       𝙏𝙞𝙥: 𝙫𝙖𝙧𝙞𝙖𝙗𝙡𝙚𝙨 𝙘𝙖𝙣𝙣𝙤𝙩 𝙗𝙚𝙜𝙞𝙣 𝙬𝙞𝙩𝙝 𝙖 𝙣𝙪𝙢𝙗𝙚𝙧                                       
+                                           ▓▓▓▓      ▓▓      ▓▓▓▓                                                              
+                                               ▓▓▓▓  ▓▓  ▓▓▓▓                                                                          
+                                                   ▓▓▓▓▓▓                                                              
+"""
+
 #Bot variable
-bot = '[𝗖𝗵𝗶𝘁-𝗖𝗵𝗮𝘁 𝗕𝗼𝘁]'
+bot = '[Chit-Chat Bot]'
 
 # prompt variable shortcuts
 prompt = '(press enter)'
@@ -52,8 +104,8 @@ prompt = '(press enter)'
 print()#(ELOC)
 print()#(ELOC)
 print()#(ELOC)
-print('                    █▀▀ █▀▀ █▄░█   █▀▀ █░█ █ ▀█▀   █▀▀ █░█ ▄▀█ ▀█▀   █▄▄ █▀█ ▀█▀')
-print('                    █▄█ ██▄ █░▀█   █▄▄ █▀█ █ ░█░   █▄▄ █▀█ █▀█ ░█░   █▄█ █▄█ ░█░')
+print('                    █▀▀ █▀▀ █▄░█   █▀▀ █░█ █ ▀█▀   █▀▀ █░█ ▄▀█ ▀█▀   █▄▄ █▀█ ▀█▀                  ')
+print('                    █▄█ ██▄ █░▀█   █▄▄ █▀█ █ ░█░   █▄▄ █▀█ █▀█ ░█░   █▄█ █▄█ ░█░                  ')
 print()#(ELOC)
 print('                                                                                                  ')
 print('                                      ░░░░░░░▄█▄▄▄█▄                                              ')
@@ -71,13 +123,14 @@ various questions that you will have to answer to gather data! Goodluck on your 
 
 lets get started!
                                                                                                        """)
-input('press enter to begin chatting!)')# waits for user to type than press enter and function call evaluates to standard input()
+input('(press enter to begin chatting!)')# waits for user to type than press enter and function call evaluates to standard input()
+print()
 
 # Question 1
 print(bot+': Hello whats your name? ')# asking a question
 name = input('(enter your name) ') # user creates variable using the input functionalities!
 print()#(ELOC)
-print(bot+':Nice to meet you '+name+' im glad to have you here today to have a fun chat with me!') # speech dialog from (Gen Chit-Chat Bot)
+print(bot+': Nice to meet you '+name+' im glad to have you here today!') # speech dialog from (Gen Chit-Chat Bot)
 print(prompt) # prompts user to press enter
 input() # Takes input w/input()
 
@@ -104,6 +157,8 @@ game = input('(enter your favorite game) ')# user creates variable using the inp
 print()#(ELOC)
 print(bot+': '+game+'? I never heard of it than again Im a bot!') # speech dialog from (Gen Chit-Chat Bot)
 input(prompt)
+print()
+
 
 #Bot Color variable
 bot_color = 'purple'
@@ -136,13 +191,51 @@ location = input('(enter your location) ')# user creates variable using the inpu
 print()
 print(bot+': I never been to '+location+' but it sounds pretty nice!') # speech dialog from (Gen Chit-Chat Bot)
 input('(press enter)') # prompts user to talk some more than press enter
+print()
+
+"""
+                               █░░█ █▀▀█ █░░░█ 　 ▀▀█▀▀ █▀▀█ 　 █▀▀ █▀▀█ █▀▄▀█ █▀▄▀█ █▀▀ █▀▀▄ ▀▀█▀▀ 
+                               █▀▀█ █░░█ █▄█▄█ 　 ░░█░░ █░░█ 　 █░░ █░░█ █░▀░█ █░▀░█ █▀▀ █░░█ ░░█░░ ✎
+                               ▀░░▀ ▀▀▀▀ ░▀░▀░ 　 ░░▀░░ ▀▀▀▀ 　 ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀░░░▀ ▀▀▀ ▀░░▀ ░░▀░░
+                 First things first you need to learn how to make single line comments and multi line comments 
+                 so you can add little reminders or give insight on what that line of code does! Comments are
+                 fun great ways to just add notes to a line so you can reflect on them later. They wont show
+                 up in the program its specifically for the developer(you) to look at! So single line comments
+                 start with a hashtag (#) and anything after that will be comments in a greyish tone. Great! Now
+                 you know how to make single line comments but what about multi line comments like this one bro?
+                 Easy all you need to do is start the line with (3 quotes) and than anything after that will be 
+                 comments and to end the finish off the line with another 3 quotes and viola! Your welcome I know 
+                 I'm awesome no need to thank me! (─‿‿─)
+                 
+                      ::---:----::/::------/::/++/::..::o:::-..------.--.::/o--:.......-:/:+:o/---:/::--..-.-:/:
+                    -:::---:----/://----::+//+o+/:-....:+/:::------:----:::s/-:-`....``.:+:+:/+---/+::--..---:+:-`
+                   /-::::--:----+///---::o+/++///::::::-:o::::----:----:::++-::-:://////+s++::o:-:/o/::------:+:-`.
+                  //::/::--::---+///-::/o+/+/::-........://:::---:------//+::/----......-/+:-/o/://o+/:------/+:-`..
+                 ::+::/:::--:--:+///:/+o++o/::---:-......:+::---::----://+/::...........-+:.`-+/:/+oo//-----://:-`..-
+                 ::+::+:::--/--:+///++o+++oyyo+//:-:::-...:+:--::----:/:/+::....`....``.-:.``./+:/o+o//-----://:-`..:
+                 ::+::o/:::-:--:+/+/+++sdhyydhhdddhs+::/-.-//-:----::-./+/-``.-:::-.`...-.```.:+/+++o+:-----///:-`.-/
+                 :///:++:::::/-:o+++oyddo::hhhhdddhddhs:-.`.+:---:--.`:+:.```---:/osyyyyyyyso/++/+//s+:----:/+::...-:
+                 ://+:+o/::::/--o/::oms:--shhyyyhhhhhhos.`.:::---.````..`````./shddhhhhhhhs/oyyss///o+----:/+//:`.-::
+                 ///+//oo::::::-/::sms-...ysyoosssyysy:/:.---..``````````````.++hhhhhyhyyhh:--:oyo//o/---://+//:`-:::
+                 ////+/oo/::::/:-/:om/..``s+oossssso-..`````````````````````...oyyssyyyyo--/---.yhh+o:--::/+///-.-::-
+                 /////oo+o/::::/-/--+s.`  o+++/ssyo++h. ```````````````````````s+osyssss/--+...:hyo/o:-::/+/:+:.-::-.
+                 /////+os+o/::::/-:-.-.   `o:-:++/--os  `````````````````````` o+/+ssss/::y/..-s/..:/--::+/:/+--:::--
+                 //////+s++o/:::/+--....   `/+-.--:++````````````````````````` .s-:-/+:.-/s  .-.`.-/:-:://:/o/-::::-/
+                 +:////++s+++o:::++:-....o/` `--:::```````````````````````````` -+....:-+o`  `..`.:+-::/+/:o+:://::++
+                  -////+++o++s//::++/-.........```....`````````````````````````` `--:://- `:.....-/:::++/:+o////::oo
+                   ://////+o/o:://:///:-..-.-.......``.``````````````````````````.------........-:/:/o///+o//////++
+                    /://///++++:://///:::----........`.``````````  ````````````............--..-://oh///+o////+/++
+                      :://///+o/::/+o////-...........`````````````:.```````````........-------://oyss:/++::++/:/
+"""
 
 #statement 1
 print(bot+': Im having so much fun talking with you '+name+'! If I remember correctly your favorite game is')# speech dialog from (Gen Chit-Chat Bot)
 print(game+' and your favorite color is '+color+'?') #speech dialog continues from line '84'
+input(prompt)
 print()# (ELOC)
 print(bot+':lets keep chatting!')# speech dialog from (Gen Chit-Chat Bot)
 input('(press enter to keep chatting)')# prompts user to keep chatting than press enter
+print()
 
 # Question 7
 print(bot+': What type of food do you like? i like seafoods and things that are very spicy at times')# asking a question
@@ -152,7 +245,8 @@ print()
 print(bot+': Oh! '+food+' sounds really good!')
 print()# (ELOC)
 print(bot+': maybe one day ill have to try it out so what was it again? '+food+' food wow!')
-input('(press enter)')# prompts user to press enter
+input(prompt)# prompts user to press enter
+print()
 
 # Question 8
 print(bot+': I have a question what time is it for you right now?')
@@ -160,6 +254,7 @@ time = input('(Enter your time) ') # user creates variable using the input funct
 print() # (ELOC)
 print(bot+': Woah! for me the time is 10:44pm! Thats when this program got created around.')
 input(prompt)# prompts user to press enter
+print()
 
 # Question 9
 print(bot+': Whats your favorite type of music? im curious like a cat!')
@@ -167,6 +262,7 @@ tunes = input('(enter your favorite artist) ') # user creates variable using the
 print() # (ELOC)
 print(bot+':Nice!')
 input(prompt)# prompts user to press enter
+print()
 
 # Question 10
 print(bot+': Did you enjoy this program? Hopefully yes!')
@@ -175,15 +271,15 @@ print() # (ELOC)
 input('(press enter to see results!)')# prompts user to press enter
 print()#(ELOC)
 print()#(ELOC)
-print('█▀█ █▀▀ █▀ █░█ █░░ ▀█▀ █▀')
-print('█▀▄ ██▄ ▄█ █▄█ █▄▄ ░█░ ▄█')
+print('                                     █▀█ █▀▀ █▀ █░█ █░░ ▀█▀ █▀                               ')
+print('                                     █▀▄ ██▄ ▄█ █▄█ █▄▄ ░█░ ▄█                               ')
 print()#(ELOC)
 print()#(ELOC)
 # Name results!
 print(bot+' gathered that your name was '+ name+'!')# Results to program by user uses w/print function
 print(prompt)
 input()
-print('You name '+name+' has '+str(len(name))+' characters in it by the way!')
+print('Your name '+name+' has '+str(len(name))+' characters in it by the way!')
 print(prompt)# prompts user to press enter
 input()# Takes input w/input()
 # Hobby results
@@ -232,7 +328,7 @@ print('                              Your name was '+name+' Thanks for playing! 
 print()#(ELOC)
 print('                              Overall did '+name+' enjoy the program?                                 ')
 print('                                          Result:'+overrall                                            )
-print('                                     (press enter to see credits)                                     ')
+print('                                    (press enter to see credits)                                      ')
 input()# Takes input w/input()
 print()#(ELOC)
 print()#(ELOC)
@@ -247,3 +343,11 @@ print('Storyline - Genesis Gir')#credit info generated by the print function
 print('Special thanks to everyone one at my twitch and follows me!')#special thanks info generated by the print function
 print('(Press ENTER to exit program)')#credit info generated by the print function
 input()#prompts user to press enter to exit program
+"""
+
+█▀█ █▀▀ █▀ █▀█ █░█ █▀█ █▀▀ █▀▀ █▀
+█▀▄ ██▄ ▄█ █▄█ █▄█ █▀▄ █▄▄ ██▄ ▄█
+
+link: >>> https://www.twitch.tv/genesisgir  <<< Find Livestreams and more!
+link: >>> https://automatetheboringstuff.com <<< Discover and learn how i did!
+"""
