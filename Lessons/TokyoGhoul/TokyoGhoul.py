4857a5c7300367a63657e72dbbd01fafccc658fe
Genesis Gir <daintygenesis@gmail.com>
2021-08-30 01:12:44 -0700

    featuring a new project named TOKYO GHOUL
    added a new .py that I'm working on has to do with the story of a girl that has the abilities to summon and control ghosts and is based off a japan settings to save her father

---
+++ Lessons/TokyoGhoul/TokyoGhoul.py
@@ -0,0 +1,64 @@
+""" 
+█▀▀ █▀▀ █▄░█ █▀▀ █▀ █ █▀   █░░ █▀▀ █▀ █▀ █▀█ █▄░█ █▀   █░█ █▀█ █░░ ░ ▄█
+█▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ ▄ ░█ 
+Welcome to the genesis gir lesson tutorials Volume 1! Tokyo ghoul is a program that takes the user in a world 
+to play as a girl named Sakura as she has the abilities to take control of the undead and control them using a 
+bloody airloom book passed down from family to family. Tokyo Ghoul is a save and rescue mission based program where
+user has to save her father from the japanese mafia and restore honor to the family , YOur mother has sent you out to
+go find and kill the men that have robbed your mother of her husband and ultimately your father. Will you save him in time?
+can you harness the will of the undead and spirits to gain your family memeber back. Rip and tear at the japanese mafia
+using ghoulish and deadly attacks from limb to limb even using a katana blade somehwere in program. Thanks for downloading~ 
+⼕ㄖᗪ🝗ᗪ & 山尺讠セセ🝗𝓝 ⻏丫 Ꮆ🝗𝓝🝗丂讠丂 Ꮆ讠尺
+"""
+
+sakura = 'Sakura (Player)'
+mother = 'jinx (Mother)'
+sofia = 'Sofia (Sister)'
+
+# This program takes the user into a blood bath based spree with chapter 1 functionalities!
+print()
+print(sakura+':My family means everything to me. .')
+print()
+print('-A zoomed in view of a childs face with her eyes closed and tears flowing down her face can be seen and as the tears flow')
+print('the scene around her is of flames and united fire all around her and dramatic soft musicing playing as her mother is')
+print('being beaten up badly and battered with a club by a japanese mafia memeber the strikes of the club can be heard against her')
+print('a view of soft glossy eyes can be seen from Sakura as they slowly blink and blink and the blood being seen flowing')
+print('from her mothers body. The mother will come to survive the attack but a view of the father being taken away as he shouts "SAKURA"')
+print('"SAKURA PAPA LOVES YOU" the gut wretching sound of flames falling from the skies and the scene shimmers like a dream')
+print('"PAPA LOVES YOU" He is taken by the mafia and shoved inside of a car. . -')
+print()
+input('(press enter to speak)')
+print(sakura+':I will find them. .The Horrors. .The pure neglect. .')
+print()
+input('press enter')
+print('--')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
