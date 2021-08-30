cdc6761a90a73437fad15331326fc538fe101699
Genesis Gir <daintygenesis@gmail.com>
2021-08-29 19:13:44 -0700

    added edits/modifications to .py

---
+++ Lessons/GenesisChitChatBot/GenChitChatBot.py
@@ -0,0 +1,252 @@
+""" 
+█▀▀ █▀▀ █▄░█ █▀▀ █▀ █ █▀   █░░ █▀▀ █▀ █▀ █▀█ █▄░█ █▀   █░█ █▀█ █░░ ░ ▄█
+█▄█ ██▄ █░▀█ ██▄ ▄█ █ ▄█   █▄▄ ██▄ ▄█ ▄█ █▄█ █░▀█ ▄█   ▀▄▀ █▄█ █▄▄ ▄ ░█
+Welcome to the Genesis Gir lesson tutorials Volume 1! Genesis Chit Chat bot is a program that gathers info on 
+the user and displays all the cool info at the end of the program as shock value that the bot could remember
+all of users fun little facts. Chit Chat bot will ask the user questions than display its results at the end of
+the program really fun to make and trying to get out of writers block right now so if its not as immersive im sorry'
+but this will be fun to make and learn from in the long run with functionalities like print, input() and even
+using multiline strings and much more! Thanks for downloading! 山尺讠〸〸🝗𝓝 & ⼕ㄖᗪ🝗ᗪ ⻏丫 Ꮆ🝗𝓝🝗丂讠丂 Ꮆ讠尺
+"""
+# Loading screen
+print('initializing bot! ')
+print('(loading. . .)')
+print('(press enter)')
+input() # T/I
+
+print('Tidying up mainframes!')
+print('(loading. . . )')
+print('(press enter)')
+input() # T/I
+
+print('Making things pretty for you!')
+print('(loading. . . )')
+print('(press enter)')
+input()
+
+print('picking up data Blocks!')
+print('(loading. . . )')
+print('(press enter)')
+input()
+
+print('Data Blocks loading in!')
+print('(loading. . .)')
+print('(press enter)')
+input()
+
+print('Letting things fall into place!')
+print('(loading. . .)')
+print('(press enter)')
+input()
+
+#Bot variable
+bot = '(Chit-Chat Bot)'
+
+# prompt variable shortcuts
+prompt = '(press enter)'
+
+
+#T This program asks the user questions and gives the results from the variables at the end 
+print()#(ELOC)
+print()#(ELOC)
+print()#(ELOC)
+print('                    █▀▀ █▀▀ █▄░█   █▀▀ █░█ █ ▀█▀   █▀▀ █░█ ▄▀█ ▀█▀   █▄▄ █▀█ ▀█▀')
+print('                    █▄█ ██▄ █░▀█   █▄▄ █▀█ █ ░█░   █▄▄ █▀█ █▀█ ░█░   █▄█ █▄█ ░█░')
+print()#(ELOC)
+print('                                                                                                  ')
+print('                                      ░░░░░░░▄█▄▄▄█▄                                              ')
+print('                                      ▄▀░░░░▄▌─▄─▄─▐▄░░░░▀▄                                       ')
+print('                                      █▄▄█░░▀▌─▀─▀─▐▀░░█▄▄█                                       ')
+print('                                      ░▐▌░░░░▀▀███▀▀░░░░▐▌                                        ')
+print('                                      ████░▄█████████▄░████                                       ')
+print()#(ELOC)
+print()#(ELOC)
+print("""
+Welcome to the Genesis Chit-Chat Bot! Ever felt bored or lonely and have nobody to talk to? 
+well designed by Genesis Gir this bot will hold converstations with you and remember all the facts you told it
+so what are you waiting for it uses simple functions like input() to gather users inputs! The bot will ask you 
+various questions that you will have to answer to gather data! Goodluck on your journey with your questions
+
+lets get started!
+(press enter to begin chatting!)
+                                                                                                       """)
+input()# waits for user to type than press enter and function call evaluates to standard input()
+
+# Question 1
+print(bot+':Hello whats your name? ')# asking a question
+name = input() # user creates variable using the input functionalities!
+print()#(ELOC)
+print(bot+':Nice to meet you '+name+' im glad to have you here today to have a fun chat with me!') # speech dialog from (Gen Chit-Chat Bot)
+print(prompt) # prompts user to press enter
+input() # Takes input w/input()
+
+# Question 2
+print(bot+': So whats your favorite thing to do '+name+'?')# asking a question
+hobby = input()# user creates variable using the input functionalities!
+print()#(ELOC)
+print(bot+':Nice that sounds interesting mine is to talk with people like you and gather data!') # speech dialog from (Gen Chit-Chat Bot)
+print(prompt) # prompts user to press enter
+input()# Takes input w/input()
+
+# Question 3
+print(bot+': Whats your mood like right now?')# asking a question
+mood = input()# user creates variable using the input functionalities!
+print()#(ELOC)
+print(bot+': Sometimes feeling '+mood+' is okay but when i find myself feeling unhappy') # speech dialog from (Gen Chit-Chat Bot)
+print('I just talk with someone about it and I feel a lot better in the end!') #speech dialog continues from line '57'
+print('(press enter)') # prompts user to press enter
+input()# Takes input w/input()
+
+# Question 4
+print(bot+': Whats your favorite game to play?')# asking a question
+game = input()# user creates variable using the input functionalities!
+print()#(ELOC)
+print(bot+': '+game+'? I never heard of it than again Im a bot!') # speech dialog from (Gen Chit-Chat Bot)
+print('(press enter)') # prompts user to press enter
+input()# Takes input w/input()
+
+#Bot Color variable
+bot_color = 'purple'
+
+# Question 5 
+print(bot+': Whats your favorite color?')# asking a question
+color = input()# user creates variable using the input functionalities!
+print()#(ELOC)
+print(bot+':'+color+'? My favorite color is '+bot_color+'!') # speech dialog from (Gen Chit-Chat Bot)
+print(prompt) # prompts user to press enter
+input()# Takes input w/input()
+
+"""
+🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂
+
+█░█ █▀█ █░█░█   ▀█▀ █▀█   █░█ █▀ █▀▀   █▀█ █▀█ █ █▄░█ ▀█▀
+█▀█ █▄█ ▀▄▀▄▀   ░█░ █▄█   █▄█ ▄█ ██▄   █▀▀ █▀▄ █ █░▀█ ░█░
+The print function always comes with two parentheses and any function in that matter and we 
+enter what data types we want inside of them and everything inside of it is called an
+argument and the print is called a function and in this case im using a (stir) or better known
+as a string to make a storyline! so print(    ) and anything inside it will be printed onto 
+stream(screen) just make sure its a correct data type: interger , float or a string value.
+
+🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 🅶🅴🅽🅴🆂🅸🆂 
+""" 
+
+# Question 6
+print(bot+': Where are you from im curious! we are friends so why not tell me!')# asking a question
+location = input()# user creates variable using the input functionalities!
+print()
+print(bot+': I never been to '+location+' but it sounds pretty and nice!') # speech dialog from (Gen Chit-Chat Bot)
+print('(press enter to talk some more)')# prompts user to talk some more than press enter
+input()# Takes input w/input()
+
+#statement 1
+print(bot+': Im having so much fun talking with you and if I remember correctly your favorite game is ')# speech dialog from (Gen Chit-Chat Bot)
+print(game+' and your favorite color is '+color+'!') #speech dialog continues from line '84'
+print()# (ELOC)
+print(bot+':lets keep chit chatting!')# speech dialog from (Gen Chit-Chat Bot)
+print('(press enter to keep chatting with your friend)')# prompts user to keep chatting than press enter
+input()# Takes input w/input()
+
+# Question 7
+print(bot+': What type of food do you like? i like seafoods and things that are very spicy at times')# asking a question
+print('fun fact spicy foods are good for your heart and it tastes good as well!')# line continues from '92' w/print
+food = input() # user creates variable using the input functionalities!
+print()
+print(bot+': Oh! '+food+' sounds really good!')
+print()# (ELOC)
+print(bot+': maybe one day ill have to try it out so what was it again? '+food+' food wow!')
+print('(press enter)') # prompts user to press enter
+input()# Takes input w/input()
+
+# Question 8
+print(bot+': I have a question what time is it for you right now?')
+print('(enter your time)')
+time = input() # user creates variable using the input functionalities!
+print() # (ELOC)
+print(bot+': Woah! for me the time is 10:44pm! Thats when this program got created around.')
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+
+# Question 9
+print(bot+': Whats your favorite type of music? im curious like a cat!')
+tunes = input() # user creates variable using the input functionalities!
+print() # (ELOC)
+print(bot+':Nice!')
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+
+# Question 10
+print(bot+': Did you enjoy this program? Hopefully yes!')
+overrall = input() # user creates variable using the input functionalities!
+print() # (ELOC)
+print('(press enter to see results!)')
+input()# Takes input w/input()
+print()
+print('█▀█ █▀▀ █▀ █░█ █░░ ▀█▀ █▀')
+print('█▀▄ ██▄ ▄█ █▄█ █▄▄ ░█░ ▄█')
+print()#(ELOC)
+print()#(ELOC)
+# Name results!
+print(bot+' gathered that your name was '+ name+'!')# Results to program by user uses w/print function
+print('(Okay! [enter])')# prompts user to press enter
+input()# Takes input w/input()
+# Hobby results
+print(bot+' gathered that your hobbies are '+hobby+'!')# Results to program by user uses w/print function
+print()
+print('NOTIFICATION!: Make sure to keep up with that hobby you never know what could happen!')# Results to program by user uses w/print function
+print('(Okay! [enter])')# prompts user to press enter
+input()# Takes input w/input()
+# Mood results
+print(bot+' discovered that your mood was '+mood+' during the session!')# Results to program by user uses w/print function
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+# Game results
+print(bot+' Discovered your favorite game to play is '+game+'!')# Results to program by user uses w/print function
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+# Color results
+print(bot+' Scanned that your favorite color was the color '+color+'!')# Results to program by user uses w/print function
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+# Location results
+print(bot+' has discovered your location is in '+location+'!')# Results to program by user uses w/print function
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+# Food Result
+print(bot+' has discovered that your favorite food was '+food+' foods! Yum sounds good!')# Results to program by user uses w/print function
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+# Time Results
+print(bot+' has learned that the time for you awhile ago was '+time+'!')# Results to program by user uses w/print function
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+# Music results
+print(bot+' remembered that your favorite type of music is '+tunes+'!')# Results to program by user uses w/print function
+print(prompt)# prompts user to press enter
+input()# Takes input w/input()
+
+# Did they enjoy the program results
+print()#(ELOC)
+print()#(ELOC)
+print('                              █▀▀ █ █▄░█ ▄▀█ █░░   █▀█ █▀▀ █▀ █░█ █░░ ▀█▀ █▀                          ')
+print('                              █▀░ █ █░▀█ █▀█ █▄▄   █▀▄ ██▄ ▄█ █▄█ █▄▄ ░█░ ▄█                          ')
+print()#(ELOC)
+print()#(ELOC)
+print('                              Your name was '+name+' Thanks for playing!                              ')
+print()#(ELOC)
+print('                              Overall did '+name+' enjoy the program?                                 ')
+print('                                          Result:'+overrall                                            )
+print('                                     (press enter to see credits)                                     ')
+input()# Takes input w/input()
+print()#(ELOC)
+print()#(ELOC)
+print('█▀▀ █▀█ █▀▀ █▀▄ █ ▀█▀ █▀')
+print('█▄▄ █▀▄ ██▄ █▄▀ █ ░█░ ▄█')
+print('Credits')#credits to yours truly
+print('programmer - genesis gir')#credit info generated by the print function
+print('Lead programmer - genesis gir')#credit info generated by the print function
+print('Program directer- genesis gir')#credit info generated by the print function
+print('Character development - Genesis Gir')#credit info generated by the print function
+print('Storyline - Genesis Gir')#credit info generated by the print function
+print('Special thanks to everyone one at my twitch and follows me!')#special thanks info generated by the print function
+print('(Press ENTER to exit program)')#credit info generated by the print function
+input()#prompts user to press enter to exit program
